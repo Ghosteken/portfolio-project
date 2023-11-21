960c833aa7ae4280e5b74c58f04d5a1445b947d7
Nicholas <aigberuan6@gmail.com>
2023-11-22 00:58:30 +0100

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Villan from "@/components/main/Villan";
 
 export default function Home() {
   return (
     <main className="h-full w-full" >
-      <div className="flex flex-col gap-20" ></div>
+      <div className="flex flex-col gap-20" >
+        <Villan/>
+      </div>
       
     </main>
   )
--- components/main/Villan.tsx
+++ components/main/Villan.tsx
@@ -1,8 +1,21 @@
 import react from 'react';
-
+ 
 const Villan = () => {
     return (
-        <div></div>
+        <div className='relative flex flex-col h-full w-full ' >
+            <video 
+            autoPlay
+            loop
+            muted
+            className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
+            >
+             <source src='/blackhole.webm' type='video/webm'/>  
+
+                
+
+            </video>
+
+        </div>
     )
 };
 
