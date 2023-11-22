4535998e30c7f7b981ab6648a65e851e3d274887
Nicholas <aigberuan6@gmail.com>
2023-11-22 02:01:22 +0100

    icons

--- components/sub/VillanContent.tsx
+++ components/sub/VillanContent.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {motion} from "framer-motion";
+import { slideInFromTop } from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/20/solid";
 
 const VillanContent = () => {
     return (
@@ -9,8 +11,11 @@ const VillanContent = () => {
         className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20] "
         >
           <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start" >
-            <motion.div> 
-                
+            <motion.div
+            variants={slideInFromTop}
+            className="Welcom-box py-[15px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+            > 
+                <SparklesIcon className="text-[#b49bff]" />
                  </motion.div>
              
             </div>  
