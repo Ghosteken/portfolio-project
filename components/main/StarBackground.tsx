55730ce37331f089624efe0380f8faed87ff7a2e
Nicholas <aigberuan6@gmail.com>
2023-11-23 11:56:10 +0100

    variants

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,20 @@
+'use clinet';
+
+import React, {useState,useRef,Suspense } from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
+import { Points, PointMaterial,Preload } from '@react-three/drei'
+//@ts-ignore
+import * as random from    'maath/random/dist/maath-random.esm'
+
+
+
+export const StarBackground = (props: any) => {
+    const ref: any = useRef()
+    const [sphere] = useState(() => 
+    random.inSphere(new Float32Array(5000),{radius:1.2})
+    );
+  return (
+    <div>StarBackground</div>
+  )
+}
+export default  StarBackground
