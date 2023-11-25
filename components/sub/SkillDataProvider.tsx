7099256aa959f3d2404c44dcde61684575892937
Nicholas <aigberuan6@gmail.com>
2023-11-25 13:09:45 +0100

    indexes

---
+++ components/sub/SkillDataProvider.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+
+import React from 'react'
+import {inView, motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+
+interface Props {
+    src: string;
+    width: number;
+    height: number;
+    index: number;
+}
+
+const SkillDataProvider = ({src,width,height,index} : Props)=> {
+    const {ref,InView} = useInView = ({
+        triggerOnce: true
+    })
+
+    const imageVariants = {
+        hidden: {opacity: 0},
+        visible: {opacity: 1},
+
+    }
+    const animationDelay = 0.3
+  return (
+    <motion.div
+    ref={ref}
+    initial = 'hidden'
+    variants={imageVariants}
+    animate={inView ? 'visible' : 'hidden'}
+    custom={index}
+    >
+
+
+    </motion.div>
+  )
+}
+
+export default SkillDataProvider
