a5f2a9125e16609fc4fd5e61052e09782ad521b1
Nicholas <aigberuan6@gmail.com>
2023-12-11 09:58:49 +0100

    edits

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             
             <span className='font-bold ml-[10px] hidden md:block text-gray-300' >
 
-                Aigberua Nicholas
+                Ghosteken
             </span>
 
 
